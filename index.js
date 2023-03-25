42d94fb7f1ce4e8f0737d539bffc219e68d0c436
SofianAcDev <sofian.achbabi@gmail.com>
2023-03-25 18:23:59 +0100

    + date calendar

--- index.js
+++ index.js
@@ -21,3 +21,25 @@ start_date.addEventListener("change", (e) => {
     end_date.value = tomorrowFormat;
   }
 });
+
+end_date.addEventListener("change", (e) => {
+  let day = new Date(e.target.value);
+
+  if (end_date.value < start_date.value) {
+    day.setDate(day.getDate() - 1);
+    start_date.value = day.toISOString().split("T")[0];
+  }
+});
+
+//Nb of days calc
+
+const bookingCalc = () => {
+  let diffTime = Math.abs(
+    new Date(end_date.value) - new Date(start_date.value)
+  );
+  let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  console.log(diffDays);
+};
+
+start_date.addEventListener("change", bookingCalc);
+end_date.addEventListener("change", bookingCalc);
